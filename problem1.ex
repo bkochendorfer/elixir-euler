00d2a5d3c0dfbe1c98dc1bf998ff64ca4cf779fb
Brett K <brettk@groupon.com>
2014-03-30 21:53:26 -0500

    First commit

---
+++ problem1.ex
@@ -0,0 +1,24 @@
+defmodule Eulixer do
+  @doc """
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below 1000.
+  """
+
+  def of(num) when rem(num, 3) == 0 do
+    num
+  end
+
+  def of(num) when rem(num, 5) == 0 do
+    num
+  end
+
+  def of(_num), do: nil
+
+  def threefive(range) do
+    Enum.filter(range, &(of &1)) |> Enum.reduce &(&1 + &2)
+  end
+
+end
+
+Eulixer.threefive 1..999
